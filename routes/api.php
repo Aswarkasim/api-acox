8eec8436423698208897e5b47fcb18a650c37ffc
aswarkasim <aswarkasim@gmail.com>
2021-11-20 08:29:38 +0700

    api a

--- routes/api.php
+++ routes/api.php
@@ -46,7 +46,7 @@ Route::group(['middleware'  => ['auth:sanctum']], function () {
 
 
     Route::resource('order', OrderApiController::class);
-    Route::put('order/get_order/{id}', [OrderApiController::class, 'get_order']);
+    // Route::put('order/get_order/{id}', [OrderApiController::class, 'get_order']);
 
 
     Route::post('/logout', [AuthController::class, 'logout']);
@@ -56,3 +56,5 @@ Route::resource('programs', ProgramController::class);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
 });
+
+Route::put('order/get_order/{id}', [OrderApiController::class, 'get_order']);
